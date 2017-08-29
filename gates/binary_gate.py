dfdfb92a7d9735a2902c0e92f1cb39a2eedd5d85
joshuapowell <joshua@viable.io>
2017-08-29 17:03:05 -0400

    Added Binary and Unary gate example classes

--- gates/binary_gate.py
+++ gates/binary_gate.py
@@ -9,4 +9,32 @@ class BinaryGate(LogicGate):
 
     Create "binary" logic gates.
     """
-    pass
+
+    def __init__(self, label):
+        """BinaryGate Constructor.
+
+        Define a binary logic gate by adding a label.
+
+        :param (object) self
+            An object representing BinaryGate
+        :param (string) label
+            A generic string that represents the LogicGate
+
+        """
+        LogicGate.__init__(self, label)
+
+        self.pinA = None
+        self.pinB = None
+
+    def getPinA(self):
+        return int(input("Enter Pin A input for gate %s: " % (self.getLabel())))
+
+    def getPinB(self):
+        return int(input("Enter Pin B input for gate %s: " % (self.getLabel())))
+
+
+"""Example Usage."""
+_gate = BinaryGate("verify")
+
+_pinA = _gate.getPinA()
+_pinB = _gate.getPinB()
--- gates/unary_gate.py
+++ gates/unary_gate.py
@@ -9,3 +9,27 @@ class UnaryGate(LogicGate):
 
     Create "unary" logic gates.
     """
+
+    def __init__(self, label):
+        """UnaryGate Constructor.
+
+        Define a unary logic gate by adding a label.
+
+        :param (object) self
+            An object representing UnaryGate
+        :param (string) label
+            A generic string that represents the LogicGate
+
+        """
+        LogicGate.__init__(self, label)
+
+        self.pinA = None
+
+    def getPinA(self):
+        return int(input("Enter Pin A input for gate %s: " % (self.getLabel())))
+
+
+"""Example Usage."""
+_gate = UnaryGate("verify")
+
+_pinA = _gate.getPinA()
